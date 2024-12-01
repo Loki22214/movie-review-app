75cdedf82f116eb3845e77a0672b04f9c873abb0
Loki22214 <misuralovro@gmail.com>
2024-12-01 14:45:59 +0100

    promjena modela Movie.cs

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieReviewApp.Models;
 
@@ -6,9 +7,11 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Rating { get; set; }
     public string Poster { get; set; }
 }
