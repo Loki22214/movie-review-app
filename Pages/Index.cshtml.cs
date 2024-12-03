d44582079731dc82b23afdc8d079bd1ae932fee8
Loki22214 <misuralovro@gmail.com>
2024-12-03 02:02:25 +0100

    implementiran sorting po ratingu filma, takoder dodana MovieDescription datoteka

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieReviewApp.Models;
 
 namespace MovieReviewApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly AppDbContext _context;
+    private string SortOrder {get; set;}
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
-    {
+     public IList<Movie> Movies { get;set; } = default!;
 
+    public void OnGet(string sortOrder)
+    {
+       // Store the sort order parameter
+        // Store the sort order parameter
+        SortOrder = sortOrder;
+
+        // Retrieve all movies from the database
+        Movies = _context.Movie.ToList();
+
+        // Perform sorting in memory
+        switch (sortOrder?.ToLower())
+        {
+            case "highestrating":
+                Movies = Movies.OrderByDescending(m => m.Rating).ToList();
+                break;
+            case "lowestrating":
+                Movies = Movies.OrderBy(m => m.Rating).ToList();
+                break;
+            default:
+                Movies = Movies.OrderBy(m => m.Title).ToList(); // Default sorting by title
+                break;
+        }
     }
 }
---
+++ Pages/MovieDescription.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieReviewApp.Models;
+
+namespace MovieReviewApp.Pages;
+
+public class MovieDescriptionModel : PageModel
+{
+    private readonly AppDbContext _context;
+
+        public MovieDescriptionModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie is not null)
+            {
+                Movie = movie;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+}
